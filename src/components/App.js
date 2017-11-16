d17435b2ecfc6271f21c38e4f462351273519cb2
himanish58 <himanish.kabiraj@gmail.com>
2017-11-16 23:23:41 +0530

    Moved the App component inside the components folder. Also created a header component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
