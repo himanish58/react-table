6cd9e094d2129d60944c38ce6ac3cffba165cfca
himanish58 <himanish.kabiraj@gmail.com>
2017-11-21 13:19:31 +0530

    Loaded the sample data to a state called details.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Header from './Header';
+import sampleData from '../sampleData';
 import Table from './Table';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    console.log(sampleData);
+    this.state = {
+      details: sampleData,
+    };
+  }
+
   render() {
     return (
       <div className="mainArea">
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,26 +1,26 @@
-module.export = {
+module.exports = {
   person1: {
     name: 'James',
     email: 'james@abc.xyz',
-    phone: 987654321,
+    phone: '987654321',
     address: 'abc st, xyz, zip- 256987',
   },
   person2: {
     name: 'Rose',
     email: 'rose@abc.xyz',
-    phone: 8897453201,
+    phone: '8897453201',
     address: 'gth st, sse, zip- 253487',
   },
   person3: {
     name: 'Bob',
     email: 'bob@abc.xyz',
-    phone: 9965874102,
+    phone: '9965874102',
     address: 'asd st, thy, zip- 412003',
   },
   person4: {
     name: 'Tom',
     email: 'tom@abc.xyz',
-    phone: 7895423600,
+    phone: '7895423600',
     address: 'yju st, qwx, zip- 320047',
   },
 };
