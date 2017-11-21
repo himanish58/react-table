060e100fa3d536e5877c3aa87a7c112bd71a1c51
himanish58 <himanish.kabiraj@gmail.com>
2017-11-21 12:29:47 +0530

    added a few file with few sample data.

---
+++ src/sampleData.js
@@ -0,0 +1,26 @@
+module.export = {
+  person1: {
+    name: 'James',
+    email: 'james@abc.xyz',
+    phone: 987654321,
+    address: 'abc st, xyz, zip- 256987',
+  },
+  person2: {
+    name: 'Rose',
+    email: 'rose@abc.xyz',
+    phone: 8897453201,
+    address: 'gth st, sse, zip- 253487',
+  },
+  person3: {
+    name: 'Bob',
+    email: 'bob@abc.xyz',
+    phone: 9965874102,
+    address: 'asd st, thy, zip- 412003',
+  },
+  person4: {
+    name: 'Tom',
+    email: 'tom@abc.xyz',
+    phone: 7895423600,
+    address: 'yju st, qwx, zip- 320047',
+  },
+};
